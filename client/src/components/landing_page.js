1f4cafa1860f5ddf8db44a5434690a99b4603eda
Harris Ahmad <24100315@lums.edu.pk>
2024-06-09 04:03:34 +0500

    refactoring

--- client/src/components/landing_page.js
+++ client/src/components/landing_page.js
@@ -1,54 +1,57 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import lc from "../images/landing_client.svg";
 import lcc from "../images/landing_coach.svg";
 import sb from "../images/speechbubble.png";
 import footer from "../images/footer.png";
-import { useNavigate } from "react-router-dom";
 import "../fonts/LeagueSpartan-VariableFont_wght.ttf";
 import "./landing_page.css";
 
 function LandingPage() {
   const navigate = useNavigate();
-  function CoachLogin() {
+
+  const handleCoachLogin = () => {
     navigate("/coach/login");
-  }
-  function Client() {
+  };
+
+  const handleClientLogin = () => {
     navigate("/client/login");
-  }
+  };
+
   return (
     <div className="first" style={{ background: "#4d5399" }}>
-      <h1 id="GH"> Guftaar</h1>
-      <h2 id="slogan"> speech made easy</h2>
-      <button id="clientsignup" onClick={Client}>
-        <img id="lc" src={lc} alt="speech support" /> I'm looking for speech
-        support{" "}
+      <h1 id="GH">Guftaar</h1>
+      <h2 id="slogan">Speech Made Easy</h2>
+      <button id="clientsignup" onClick={handleClientLogin}>
+        <img id="lc" src={lc} alt="speech support" />
+        I'm looking for speech support
       </button>
-      <button id="coachlogin" onClick={CoachLogin}>
-        <img id="lcc" src={lcc} alt="coach" /> I'm a coach that offers services{" "}
+      <button id="coachlogin" onClick={handleCoachLogin}>
+        <img id="lcc" src={lcc} alt="coach" />
+        I'm a coach that offers services
       </button>
       <a id="admin" href="/admin/login">
-        {" "}
         Admin Login
       </a>
-      <h1 id="abt"> about us</h1>
-      <img id="sb" src={sb} alt="speech bubble"></img>
+      <h1 id="abt">About Us</h1>
+      <img id="sb" src={sb} alt="speech bubble" />
       <p className="info" align="left">
         Guftaar is a speech therapy support web service, empowering PWS to take
         the first step in reclaiming their speech. With guided vocal practice,
-        daily exercises and positive reinforcement, through Guftaar, we want to
+        daily exercises, and positive reinforcement, through Guftaar, we want to
         make it easier for you to overcome your stuttering.
         <br />
-        Beyond independent speech practice, Guftaar connects users to top rated
-        coaches for one-one mentorship calls, and brings exclusive access to
+        Beyond independent speech practice, Guftaar connects users to top-rated
+        coaches for one-on-one mentorship calls, and brings exclusive access to
         purchase complete stammer support courses.
         <br />
         At Guftaar, we help make speech easy.
       </p>
-      <h1 id="sj"> Take charge of your speech journey</h1>
+      <h1 id="sj">Take Charge of Your Speech Journey</h1>
       <a id="cta" href="/client/register">
-        {" "}
-        sign up now
+        Sign Up Now
       </a>
-      <img id="footer" src={footer} alt="main-footer"></img>
+      <img id="footer" src={footer} alt="main-footer" />
     </div>
   );
 }
