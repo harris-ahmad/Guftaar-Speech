acbd60863e53259a4087185c6ed8dec01bd2e030
harris-ahmad <24100315@lums.edu.pk>
2024-06-16 05:37:54 +0500

    responsive

--- client/src/components/landing_page.js
+++ client/src/components/landing_page.js
@@ -7,35 +7,31 @@ import footer from "../images/footer.png";
 import "../fonts/LeagueSpartan-VariableFont_wght.ttf";
 import "./landing_page.css";
 
-function LandingPage() {
+const LandingPage = () => {
   const navigate = useNavigate();
 
-  const handleCoachLogin = () => {
-    navigate("/coach/login");
-  };
-
-  const handleClientLogin = () => {
-    navigate("/client/login");
+  const navigateTo = (path) => () => {
+    navigate(path);
   };
 
   return (
-    <div className="first" style={{ background: "#4d5399" }}>
+    <div className="first">
       <h1 id="GH">Guftaar</h1>
       <h2 id="slogan">Speech Made Easy</h2>
-      <button id="clientsignup" onClick={handleClientLogin}>
-        <img id="lc" src={lc} alt="speech support" />
-        I'm looking for speech support
-      </button>
-      <button id="coachlogin" onClick={handleCoachLogin}>
-        <img id="lcc" src={lcc} alt="coach" />
-        I'm a coach that offers services
-      </button>
-      <a id="admin" href="/admin/login">
-        Admin Login
-      </a>
+      <div className="buttons-container">
+        <button id="clientsignup" onClick={navigateTo("/client/login")}>
+          <img id="lc" src={lc} alt="speech support" />
+          I'm looking for speech support
+        </button>
+        <button id="coachlogin" onClick={navigateTo("/coach/login")}>
+          <img id="lcc" src={lcc} alt="coach" />
+          I'm a coach that offers services
+        </button>
+      </div>
+      {/* <a id="admin" href="/admin/login">Admin Login</a> */}
       <h1 id="abt">About Us</h1>
       <img id="sb" src={sb} alt="speech bubble" />
-      <p className="info" align="left">
+      <p className="info">
         Guftaar is a speech therapy support web service, empowering PWS to take
         the first step in reclaiming their speech. With guided vocal practice,
         daily exercises, and positive reinforcement, through Guftaar, we want to
@@ -48,12 +44,10 @@ function LandingPage() {
         At Guftaar, we help make speech easy.
       </p>
       <h1 id="sj">Take Charge of Your Speech Journey</h1>
-      <a id="cta" href="/client/register">
-        Sign Up Now
-      </a>
+      <a id="cta" href="/client/register">Sign Up Now</a>
       <img id="footer" src={footer} alt="main-footer" />
     </div>
   );
-}
+};
 
 export default LandingPage;
